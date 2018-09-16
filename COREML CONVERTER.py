10ad02bb622434f591c117f86861f105ca86bb6c
Steven Feng <stevenyfeng@gmail.com>
2018-09-16 08:34:22 -0400

    LAST FILES - SciKit & CoreML

--- COREML CONVERTER.py
+++ COREML CONVERTER.py
@@ -1,8 +1,10 @@
 import coremltools
 
-coreml_model = coremltools.converters.keras.convert('my_model.h5')
+input_features = ["symptoms"]
+output_feature = "medical condition"
+model = coremltools.converters.keras.convert('my_model.h5',input_features,output_feature)
 print("successfully loaded keras model")
 
-coreml_model.save('HeightWeight_model.mlmodel')
+model.save('HeightWeight_model.mlmodel')
 #coremltools.utils.save_spec(coreml_model, 'my_model.mlmodel')
 print("successfully saved coreML model")
